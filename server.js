9478519646233a16309c12a6721ed9b3a28e1fc6
cheezm91 <cheezm91@gmail.com>
2018-05-17 18:42:07 -0400

    fixed mysqlssh connection

--- server.js
+++ server.js
@@ -3,46 +3,44 @@ const bodyParser = require('body-parser');
 const app = express();
 const React = require('react');
 const mysqlssh = require('mysql-ssh');
-const config = require('./config.js');
+const config = require('./config.js'); //import connection data
 
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-var connection = mysqlssh.connect(
-  {
-      host: config.sshpw.host,
-      port: config.sshpw.port,
-      username: config.sshpw.username,
-      password: config.sshpw.password
-  },
-  {
-      host: config.sqlpw.host,
-      user: config.sqlpw.user,
-      password: config.sqlpw.password,
-      database: config.sqlpw.database
-  }
-);
+//Make Connection
+function connection(){
+  return(
+    mysqlssh.connect(
+      {
+          host: config.sshpw.host,
+          port: config.sshpw.port,
+          username: config.sshpw.username,
+          password: config.sshpw.password
+      },
+      {
+          host: config.sqlpw.host,
+          user: config.sqlpw.user,
+          password: config.sqlpw.password,
+          database: config.sqlpw.database
+      }
+    )
+  );
+}
 
-connection.then(client => {
+//Test Connection
+connection().then(client => {
   client.query('SELECT * FROM Trains', function (err, results, fields) {
     if (err) throw err
     console.log(results);
+    mysqlssh.close();
   })
-  .catch(err => {
-    console.log(err)
-  });
+})
+.catch(err => {
+  console.log(err)
 });
 
-connection.then(client => {
-  client.query('SELECT * FROM Segments', function (err, results, fields) {
-    if (err) throw err
-    console.log(results);
-  })
-  .catch(err => {
-    console.log(err)
-  });
-});
-
+//Listen to Port 
 app.listen(3001, function() {
     console.log("Server running on 3001");
 });
