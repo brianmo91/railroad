9724347ac04af0ebc940662a391d40338e31be50
cheezm91 <cheezm91@gmail.com>
2018-05-18 02:09:13 -0400

    css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Route, Link, BrowserRouter } from 'react-router-dom';
 import './App.css';
+import logo from './logo.svg';
+
+import Train from './Train';
+
+class Routes extends Component{
+  render(){
+    return(
+      <Link to="/">Home</Link>
+    );
+  }
+
+}
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+            <Routes/>
+          </header>
+          <Route path="/" exact component={ Train } />
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ src/Train.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+
+class TrainSelection extends Component{
+
+}
+
+class TrainList extends Component{
+
+}
+
+class Train extends Component{
+  render(){
+    return(
+      <div>
+          <Link to="/">Home</Link>
+      afds
+      </div>
+    );
+  }
+
+}
+
+export default Train;
